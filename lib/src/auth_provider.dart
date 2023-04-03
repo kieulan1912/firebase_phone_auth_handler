fb8ad44cd2d3c1538a0c1736870a2a44126a5c1f
Kiều Văn Lân <kieulan826@gmail.com>
2023-04-03 20:26:50 +0700

    init prj

---
+++ lib/src/auth_provider.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_phone_auth_handler/src/auth_handler.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+/// Wrap the [MaterialApp] with [FirebasePhoneAuthProvider]
+/// to enable your application to support phone authentication.
+class FirebasePhoneAuthProvider extends StatelessWidget {
+  const FirebasePhoneAuthProvider({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  /// The child of the widget.
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<FirebasePhoneAuthController>(
+      create: (_) => FirebasePhoneAuthController(),
+      child: child,
+    );
+  }
+}
